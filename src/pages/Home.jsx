5a373073367c469ebf5fb8354dd2b744f08d24a0
Faysal07 <faysalraktimurlmain@gmail.com>
2026-04-22 23:51:17 +0600

    Home page & CSS Work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
 
 function Home () {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect (() => {
+        fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(data => setProducts(data.products))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
-        <h1>You are now Home Page.</h1>
+        <div>
+            <div className="banner_section">
+                <h1 className="banner_title">Welcome to <span>easybuy</span> Shop</h1>
+                <p className="banner_sologan">Discover amazing products at great price.</p>
+            </div>
+            <div className="container">
+                <div className="product_title">
+                    <h2>Our Products</h2>
+                </div>
+                <div className="product_details">
+
+                    {products.map((product) => (
+                    
+                        <div className="product_card" key={product.id}>
+                            <div className="product_card_image">    
+                                <img src={product.thumbnail} alt="" />
+                            </div>
+                            <div className="product_card_info">
+                                <h3 className="product_card_title">{product.title}</h3>
+                                <p className="product_card_desc">{product.description}</p>
+                                <p className="preoduct_card_price">Price: <span>$ {product.price}</span></p>
+                                <p className="product_card_category">Category: <span>{product.category}</span></p>
+                                <p className="preoduct_card_stock">Stock: <span> {product.stock}</span></p>
+                                <p className="product_card_rating">{product.rating}</p>
+                                {product.stock < 1 ? <h3 className="product_card_title">Out of stock</h3> : <h3 className="product_card_title">In stock</h3>}
+                            
+                                <Link to="" className="single_product_info">View this Product</Link>
+                                {product.stock < 1 ? <Link to="" className="addToCard">Not possible to Card</Link> : <Link to="" className="addToCard">Add to Card</Link>}
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+        </div>
     );
 }
 
